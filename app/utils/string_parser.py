4e51cc149127585c4805ccab3f77a6d396e6cee3
ytuik <ytuik99@gmail.com>
2026-04-26 19:22:23 -0300

    Ajustando o formatter de data

--- app/utils/string_parser.py
+++ app/utils/string_parser.py
@@ -4,11 +4,11 @@ from datetime import date
 def parse_data(valor: str) -> date:
     """
     Converte uma string de data para objeto date.
-    Aceita DD-MM-AAAA, DD/MM/AAAA, AAAA-MM-DD e AAAA/MM/DD para ser tolerante a variações.
+    Aceita DD-MM-AAAA, DD/MM/AAAA, DD/MM/AA,  AAAA-MM-DD e AAAA/MM/DD para ser tolerante a variações.
     """
     valor = valor.strip()
 
-    for fmt in ("%d-%m-%Y", "%d/%m/%Y", "%Y-%m-%d", "%Y/%m/%d"):
+    for fmt in ("%d-%m-%Y", "%d/%m/%Y", "%d/%m/%y" , "%Y-%m-%d", "%Y/%m/%d"):
         try:
             from datetime import datetime
             return datetime.strptime(valor, fmt).date()
