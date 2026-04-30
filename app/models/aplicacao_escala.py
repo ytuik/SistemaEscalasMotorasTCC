3bc3b287e32718a0a0d32256fafe7242321704bb
ytuik <ytuik99@gmail.com>
2026-04-30 12:01:29 -0300

    Lint

--- app/models/aplicacao_escala.py
+++ app/models/aplicacao_escala.py
@@ -9,14 +9,14 @@ class AplicacaoEscala(Base):
     id_avaliacao = Column(Integer, ForeignKey('avaliacao.id'), nullable=False)
     id_escala = Column(Integer, ForeignKey('escala.id'), nullable=False)
     pontuacao_total = Column(Integer, nullable=False, default=0)
-    
+
     avaliacao = relationship("Avaliacao", back_populates="aplicacoes_escala")
     escala = relationship("Escala", back_populates="aplicacoes_escala")
-    respostas = relationship("RespostaItem" , back_populates="aplicacao_escala", cascade="all, delete-orphan")
-    
+    respostas = relationship("RespostaItem", back_populates="aplicacao_escala", cascade="all, delete-orphan")
+
     def calcular_pontuacao_total(self):
         """Voltar aqui porque a pontuação total deve ser calculada a partir das escalas que foram usadas"""
         self.pontuacao_total = sum(resposta.pontuacao for resposta in self.respostas if resposta.pontuacao is not None)
-    
+
     def __repr__(self):
         return f"<AplicacaoEscala(id={self.id}, id_avaliacao={self.id_avaliacao}, id_escala={self.id_escala}, pontuacao_total={self.pontuacao_total})>"
