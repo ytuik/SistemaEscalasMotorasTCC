7eb06d43def9ba9f2f9166d2f2e49b05d5657236
ytuik <ytuik99@gmail.com>
2026-04-30 11:54:43 -0300

    Corrigindo a relação entre fisioterapeuta e Avaliacao

--- app/models/avaliacao.py
+++ app/models/avaliacao.py
@@ -12,8 +12,8 @@ class Avaliacao(Base):
     observacoes = Column(Text, nullable=True)
     
     paciente = relationship("Paciente", back_populates="avaliacoes")
-    fisioterapeuta = relationship("Fisioterapeuta")
     aplicacoes_escala = relationship("AplicacaoEscala", back_populates="avaliacao", cascade="all, delete-orphan")
+    fisioterapeuta = relationship("Fisioterapeuta", back_populates="avaliacoes")
     
     def __repr__(self):
         return f"<Avaliacao(id={self.id}, id_paciente={self.id_paciente}, id_fisioterapeuta={self.id_fisioterapeuta}, data={self.data})>"
--- app/models/fisioterapeuta.py
+++ app/models/fisioterapeuta.py
@@ -1,5 +1,6 @@
 
 from sqlalchemy import Column, Integer, String
+from sqlalchemy.orm import relationship
 from app.database import Base
 
 
@@ -11,6 +12,9 @@ class Fisioterapeuta(Base):
     nome = Column(String(255), nullable=False)
     email = Column(String(255), nullable=False, unique=True)
     registro_crefito = Column(String(255), nullable=False, unique=True)
+    
+    avaliacoes = relationship("Avaliacao", back_populates="fisioterapeuta")
+
     
     def __repr__(self):
         return f"<Fisioterapeuta(id={self.id}, nome='{self.nome}', email='{self.email}', registro_crefito='{self.registro_crefito}')>"
